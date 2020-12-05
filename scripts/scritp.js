74057f4ae100798c0db8eafec9afc7749f30b4af
Daniil Leynov <brianferguson1984.456@gmail.com>
2020-12-05 17:36:34 +0300

    first logic

--- scripts/scritp.js
+++ scripts/scritp.js
@@ -1,5 +1,31 @@
-const arr = ['hello world', 'men', "very nice"];
-
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i] + ` номер ${i}`);
+const numberSqr = prompt("Введите число");
+const sqrtNum1 = Math.sqrt(numberSqr);
+// console.log(sqrtNum1);
+const mass = [];
+const res1mass = [];
+const boolNumber = Number.isInteger(sqrtNum1);
+if (boolNumber === false){
+    for (let i = 2; i <= 99; i++){
+       mass[i] = i*i;
+       //console.log(mass[i]);
+    }
+    for (let i = 0; i <= mass.length; i++){
+        let res1 = numberSqr / mass[i];
+        
+        
+        //console.log(res1);
+        if (Number.isInteger(res1) === true){
+            res1mass[i] = res1;
+            
+            let res1max = Math.max(res1mass[i]);
+            //console.log(res1max);
+            let res2 = numberSqr / res1max;
+            console.log(`${Math.sqrt(res2)}√${res1max}`);
+            //break;
+        }else{
+            console.log(sqrtNum1);
+        }
+    }
 }
+
+
